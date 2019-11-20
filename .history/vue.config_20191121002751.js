b88510343318b0b9a253a1bbbc29e368ee939e72
unknown <374792302@qq.com>
2019-11-21 00:37:16 +0800

    2

---
+++ .history/vue.config_20191121002751.js
@@ -0,0 +1,13 @@
+// vue.config.js
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule('eslint')
+      .use('eslint-loader')
+      .loader('eslint-loader')
+      .tap(options => {
+        options.fix = true
+        return options
+      })
+  }
+}
